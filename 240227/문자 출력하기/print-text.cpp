8a82ca4f11d4f8aae9be60583193578720865152
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-27 17:11:27 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/문자 출력하기/print-text.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char n;
+    cin >> n;
+    for (int i=1; i<=8;i++){
+        cout<< n;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
