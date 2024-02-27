502cc6ab0da33f46788cffd566f03af1e5699e39
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-27 15:26:28 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,i=1;
+    cin>>n;
+    while(i<=n){
+        cout<<i<<" ";
+        i=i+1;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
