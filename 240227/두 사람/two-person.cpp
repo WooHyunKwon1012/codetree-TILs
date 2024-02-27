c5947c2578c857a503398d15babc304be82481b9
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-27 12:06:49 +0900

    Title: 두 사람, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240227/두 사람/two-person.cpp
+++ 240227/두 사람/two-person.cpp
@@ -5,8 +5,10 @@ int main() {
     char a_sex,b_sex;
     cin >>a_age>>a_sex;
     cin >> b_age>>b_sex;
-    if ((a_age>=19 || b_age>=19) && (a_sex == 'M' || b_sex =='M')){
-        cout <<1;
+    if ((a_age>=19 || b_age>=19)){
+        if(a_sex == 'M' || b_sex =='M'){
+            cout <<1;
+        }
     }
     else{
         cout<<0;
