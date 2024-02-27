4f64ede2eaaa03e4b5cbb69b27c07563be9a5fb4
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-27 11:02:08 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/굉장한 숫자/amazing-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >>a;
+    if ((a%2==1 && a%3==0) || (a%2==0 && a%5==0)){
+        cout << "true";
+    }
+    else {
+        cout << "false";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
