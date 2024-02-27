2005afd8f944be243cd027c5fd7b6c229779cf8d
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-27 15:02:21 +0900

    Title: 정수 입력받아 배수 출력, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int a;
+    cin>>n;
+    for(int i=1; i<=5;i++){
+        a=n*i;
+        cout << a<<" ";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
