9463db170d24a6fb09e23f7a6b7648d7e38f7693
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-27 15:29:57 +0900

    Title: 별 n개 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int n,p=1;
+    string i="*";
+    cin>>n;
+    while (p<=n){
+        cout<< i<<endl;
+        p=p+1;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
