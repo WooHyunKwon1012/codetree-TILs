7d82fe3e1dab795ce0917c961ccdbddfe5c88b9a
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-28 09:12:53 +0900

    Title: 369 게임, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240228/369 게임/369-game.cpp
+++ 240228/369 게임/369-game.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 int main() {
     int n;
-    cin>>n;
-    for(int i=1; i<=n;i++){
-      int A= i/10;
-      int B= i-10*A;
-      if(i%3==0){
-        cout<<0<<" ";
-      }
-      else if (i>=10){
-        cout<< ((A%3==0 || B%3==0)?0:i) <<" ";
-      }
-      else{
-        cout<<i<<" ";
-      }
-      
+    cin >> n;
+    
+    for (int i=1; i<=n; i++){
+        int A= i/10;
+        int B= i-A;
+        if (i%3==0){
+            cout<< 0<<" ";
+        }
+        else if(i>=10){
+            cout << (A%3==0 || B%3==0) ? 0:i;
+            cout<< " ";
+        }
+        else{
+            cout << i<<" ";
+        }
     }
     // 여기에 코드를 작성해주세요.
     return 0;
