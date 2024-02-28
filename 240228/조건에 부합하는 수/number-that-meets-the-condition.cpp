0559b5a20888ccbc5eb6dce63fc85e5305ae804e
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-28 18:49:34 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240228/조건에 부합하는 수/number-that-meets-the-condition.cpp
+++ 240228/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main() {
     int a;
-    cin>>a;
+    cin >> a;
     for(int i=1; i<=a; i++){
+       
         if(i%2==0 && i%4!=0){
             continue;
         }
-        else if(i/8 == 2 || i/8 ==4 || i/8 ==6 || i/8 ==8 || i/8 ==0){
+        else if(i/8 == 2 || i/8 ==4 || i/8 ==6 || i/8 ==0){
             continue;
         }
-        else if(i%7<4){
-            
+        else if(i%7 < 4){
             continue;
         }
         else{
@@ -19,6 +19,7 @@ int main() {
         }
         
     }
+   
     // 여기에 코드를 작성해주세요.
     return 0;
 }
