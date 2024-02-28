dc79e7b3eb44a4eb0bf984b55593b27abb9060f6
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-28 17:34:24 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240228/a의 b승/a-to-the-power-of-b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,sum=1;
+    cin>>a>>b;
+    for(int i=1; i<=b;i++){
+        sum=sum*a;
+    }
+    cout<<sum;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
