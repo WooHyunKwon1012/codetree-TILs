bb1e0d8bdb294314e561a70d72d2c0ba2972cd1e
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-28 18:41:02 +0900

    Title: 1부터의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240228/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,sum=0;
+    cin>>n;
+    for (int i=1; i<=n; i++){
+        sum=sum+i;
+        if(sum>=n){
+            cout<<i;
+            break;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
