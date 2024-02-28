4b9e67ae1f51d25c096ac97be6b774a9f442a86b
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-28 10:06:18 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240228/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int cnt_class=0,cnt_hall=0,cnt_toilet=0;
+    int n;
+    cin>>n;
+    for (int i=1; i<=n; i++){
+        if(i%12==0){
+            cnt_toilet++;
+        }
+        else if(i%3==0){
+            cnt_hall++;
+        }
+        else if(i%2==0){
+            cnt_class++;
+        }
+
+    }
+    cout<< cnt_class<<" "<<cnt_hall<<" "<<cnt_toilet;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
