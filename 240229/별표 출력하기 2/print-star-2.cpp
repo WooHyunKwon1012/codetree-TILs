bef29bc6f601b94a321c3f7a85878be825433f5f
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-29 18:08:00 +0900

    Title: 별표 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/별표 출력하기 2/print-star-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    
+    
+    for(int k=n;k>=1; k--){
+        for(int i=k; i>=1; i--){
+            cout<<"*"<<" ";
+        }
+    cout<<endl;
+    }
+    
+    
+
+        
+    
+    
+    return 0;
+}
