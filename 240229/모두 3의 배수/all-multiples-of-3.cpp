9b38dac2eed5be4471aa7d7e0092f148515628b2
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-02-29 14:54:54 +0900

    Title: 모두 3의 배수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240229/모두 3의 배수/all-multiples-of-3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c,d,e;
+    bool satis=true;
+    cin>>a>>b>>c>>d>>e;
+    if(a%3==0){
+        if(b%3==0){
+            if(c%3==0){
+                if(d%3==0){
+                    if(e%3==0){
+                        satis=true;
+                    }
+                }
+            }
+        }
+    }
+    
+    if(satis==true){
+        cout<<1;
+    }
+    else{
+        cout<<0;
+    }
+    
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
