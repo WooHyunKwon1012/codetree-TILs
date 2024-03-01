29f33b2e26095bfd6ff82b6ba7f7abcf6fe636f3
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-03-01 12:04:56 +0900

    Title: 별표 출력하기 6, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240301/별표 출력하기 6/print-star-6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=n-i;j++){
+            cout<<"  ";
+        }
+        for(int k=1; k<=2*i-1;k++){
+            cout<<"* ";
+        }
+        
+        cout<<endl;    
+    }
+    
+    for(int i=n-1; i>=1; i--){
+        for(int j=i-n+3; j>=1;j--){
+            cout<<"  ";
+        }   
+        for(int k=1; k<=2*(n-i)+1;k++){
+            cout<<"* ";
+        }
+         
+        cout<<endl;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
