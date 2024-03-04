6c988ca571785d2d555bfbb90db50fd7f4a9778d
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-03-04 19:19:14 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240304/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    char cnt='A';
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i<=j){
+                
+                cout<<cnt<<" ";
+                cnt++;
+                if(cnt=='Z'+1){
+                    cnt='A';
+                }
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
