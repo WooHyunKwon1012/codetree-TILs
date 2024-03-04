2411b091f5124e1024a226304cf100bf4700d792
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-03-04 15:28:34 +0900

    Title: 9-1 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240304/9-1 정사각형/9-1-square.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,cnt=9;
+    cin>> n;
+    for(int i=1;i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cout<<cnt;
+            cnt-=1;
+            if(cnt==0){
+                cnt=9;
+            }
+        }
+        cout<<endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
