020ed766ccdb5508b780e1b4cc8ad930e9483d9a
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-03-08 09:19:43 +0900

    Title: 나눗셈의 나머지, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240308/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b, cnt[10]={},c,d,sum=0;
+    cin>>a>>b;
+    for(int i; i<100; i++){
+        c=a/b;
+        d=a%b;
+        a=c;
+        cnt[d]++;
+        if(a<=1){
+            break;
+        }
+        
+    }
+    
+    for(int i=0;i<b;i++){
+        sum=sum+cnt[i]*cnt[i];
+    }
+    
+    cout<<sum;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
