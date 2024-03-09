309e2a045ac00f0abda755e023667a607756c91c
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-03-09 15:50:31 +0900

    Title: n개의 숫자 중 최대 2개, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240309/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
+++ 240309/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n, arr[100],max2,max,cnt=0;
+    int n, arr[100],max2,max,tmp;
     cin>>n;
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
-    max2=arr[0];
+    
     max=arr[0];
-    for(int i=1; i<n; i++){
-        
-        if(arr[i]>=max){
+    max2=arr[1];
+    if(max<max2){
+        max=arr[1];
+        max2=arr[0];
+    }
+
+    for(int i=2; i<n; i++){
+        if(arr[i]>max){
+            
             max=arr[i];
         }
-        else if(arr[i]<max && arr[i]>=max2){
+        else if(arr[i]>max2){
             max2=arr[i];
         }
+        
+        
     }
-    for(int i=0; i<n; i++){
-        if(arr[i]==max){
-            cnt++;
-        }
-    }
-    if(cnt>=2){
-        cout<< max<<" "<<max;
-    }
-    else{
-        cout<< max<<" "<<max2;
-    }
-    
+
+    cout<< max<<" "<<max2;
+   
     
     
     // 여기에 코드를 작성해주세요.
