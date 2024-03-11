8cfd3389088d4333ac79393c111da39c6154a4c8
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-03-11 18:57:07 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240311/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,arr[1000]={},prize=0;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=n-1; i>=0; i--){
+        for(int j=0; j<n; j++){
+            if(i>j && (arr[i]-arr[j])>prize){
+                prize=arr[i]-arr[j];
+            }
+        }
+    }
+    cout<<prize;
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
