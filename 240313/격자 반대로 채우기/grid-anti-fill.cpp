8bef347675f6046d042d91510c24e185680bbba8
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-03-13 16:01:58 +0900

    Title: 격자 반대로 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240313/격자 반대로 채우기/grid-anti-fill.cpp
+++ 240313/격자 반대로 채우기/grid-anti-fill.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main() {
     int n,arr[10][10],cnt=1;
     cin>>n;
+    if(n%2==0){
     for(int j=n-1; j>=0; j--){
         
             if(j%2==1){
@@ -19,6 +20,26 @@ int main() {
                 
             }
         
+        }
+    }
+    else{
+        for(int j=n-1; j>=0; j--){
+        
+            if(j%2==0){
+                for(int i=n-1; i>=0; i--){
+                arr[i][j]=cnt;
+                cnt++;
+                }
+            }
+            else{
+                for(int i=0; i<n; i++){
+                arr[i][j]=cnt;
+                cnt++;
+                }
+                
+            }
+        
+        }
     }
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
