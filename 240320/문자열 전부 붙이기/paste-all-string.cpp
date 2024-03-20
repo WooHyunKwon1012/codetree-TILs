6269203d919414cfc4f727c69fd000f50570c658
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2024-03-20 11:12:45 +0900

    Title: 문자열 전부 붙이기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240320/문자열 전부 붙이기/paste-all-string.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    string str[10]={};
+    for(int i=0; i<n; i++){
+        cin>>str[i];
+        cout<<str[i];
+    }
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
